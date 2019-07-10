e14c4c358ce34c01aa24cc24d14b8326786b98da
Miri <mirimarkovitzbl@gmail.com>
2019-07-10 09:04:16 -0700

    add functionality plugin, add custom post type of product, add hero image to about page, add logo to header.

--- themes/Inhabitant-theme/header.php
+++ themes/Inhabitant-theme/header.php
@@ -17,11 +17,13 @@
 	</head>
 
 	<body <?php body_class(); ?>>
+	
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
+					<img src=" <?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-tent-white.svg" alt="inhabitent  white logo tent" width="50" hight="50">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
@@ -30,6 +32,10 @@
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
+				
 			</header><!-- #masthead -->
-
+			<?php if ( has_post_thumbnail() ) {
+					the_post_thumbnail(); 
+					} ?>
+			
 			<div id="content" class="site-content">

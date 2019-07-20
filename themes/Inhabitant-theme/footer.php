a7657d0b80e398603343acce4216d5c71730e53b
Miri <mirimarkovitzbl@gmail.com>
2019-07-20 16:11:38 -0700

    style the footer and add the background image for it.

--- themes/Inhabitant-theme/footer.php
+++ themes/Inhabitant-theme/footer.php
@@ -9,15 +9,19 @@
 
 			</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
+			<footer style="background: url(<?php echo get_stylesheet_directory_uri() . '/images/dark-wood.png'; ?>)" id="colophon" class="site-footer" role="contentinfo">
 				<div id="footer-sidebar" class="secondary">
 					<div id="footer-sidebar1" class="footer-sidebar1">
-					<?php
-					if(is_active_sidebar('footer-sidebar-1')){
-					dynamic_sidebar('footer-sidebar-1');
-					}
-					?>
+						<?php
+						if(is_active_sidebar('footer-sidebar-1')){
+						dynamic_sidebar('footer-sidebar-1');
+						}
+						?>
+						<div class="footer-logo">
+							<a href="//localhost:3000/Inhabitant-Project4/"><img src=" <?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-text.svg" alt="inhabitent logo text"></a>
+						</div>
 					</div>
+
 				</div>
 				<div class="site-info">
 					<?php printf( esc_html( 'COPYRIGHT © 2019 INHABITENT' ), 'WordPress' ); ?>
